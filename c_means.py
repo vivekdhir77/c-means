8424fa4d460c3419b452f96e2c903e023160df7a
vivekdhir77 <vivekdhir77@gmail.com>
2023-02-08 12:03:08 +0530

    fuzzy c means

---
+++ c_means.py
@@ -0,0 +1,48 @@
+import numpy as np 
+import pandas as pd 
+import random
+from utils import *
+df = [
+    [5.1, 3.5, 1.4, 0.2],
+    [4.9, 3, 1.4, 0.2],
+    [4.7, 3.2, 1.3, 0.2],
+    [7, 3.2, 4.7, 1.4],
+    [6.4, 3.2, 4.5, 1.5],
+    [6.9, 3.1, 4.9, 1.5],
+    [5.6, 2.7, 4.2, 1.3],
+    [6.3, 3.3, 6, 2.5],
+    [5.8, 2.7, 5.1, 1.9],
+    [7.1, 3, 5.9, 2.1],
+     ] # points
+
+k = 3 # no of clusters
+m = 2 # membership value
+dataSize = len(df) # no of data points
+c = random.sample(df, k) # random k centroids
+epsilon = 1 # stopping criteria 1
+# print(c)
+
+dist = [[0 for i in range(dataSize)] for j in range(k)] # 2d Distance matrix
+mem = [[0 for i in range(dataSize)] for j in range(k)] #initializing memebership matrix
+
+
+while True:
+    dist = calcDistMatrix(dist, c, df) # calculating distance values
+    mem = caclMembershipMatrix(mem ,dist ,dataSize ,k, m) # calculating membership values
+    # print(dist)
+    mem_final = [mem[0][i]+mem[1][i]+mem[2][i] for i in range(len(mem[0]))] 
+    print(mem_final)
+    new_centroids = calcNewCentroid(df, mem, dataSize, k, m)
+    # print(new_centroids)
+    obj_func = calcObjFunction(df,mem, new_centroids, dataSize, k, m) # objective funtion between new centroids and datapoints
+    old_obj_func = calcObjFunction(df,mem, c, dataSize, k, m) # objective function between old centroids and datapoints
+    norm_obj_fun = (old_obj_func-obj_func)
+    # print(norm_obj_fun)
+    center_diff = [math.sqrt(norm_square(new_centroids[i], c[i])) for i in range(k)]
+    # print(center_diff)
+    if stoppingCriteria2(center_diff) or norm_obj_fun<epsilon:
+        break
+    c = new_centroids
+print(new_centroids)
+print("\n\n")
+print(mem)
---
+++ utils.py
@@ -0,0 +1,65 @@
+import math
+def getDistance(centroid, point):
+    dist = 0;
+    for i in range(len(point)):
+        dist += ((point[i]-centroid[i])**2)
+    return math.sqrt(dist) # distance between 2 points
+
+def memb(dist, i,j,m): # formula for membership
+    numerator = 0 
+    if(dist[j][i]!=0):
+        numerator = (1/dist[j][i])**(1/(m-1))
+    else:
+        return 0
+    denominator=0
+    for k in range(len(dist)):    
+        if(dist[k][i]!=0):
+            denominator += (1/dist[k][i])**(1/(m-1)) 
+    return numerator/denominator 
+
+def norm_square(x,c):
+    ans = 0
+    for i in zip(x,c):
+        ans+=((i[0]-i[1])**2)
+    return ans
+
+def calcNewCentroid(df, mem, dataSize, k, m):
+    new_centroid = []
+    for j in range(k):
+        new_center = [0 for i in range(len(df[0]))] # the numerator part for calculating new centroid
+        denominator = 0
+        for i in range(dataSize):
+            new_center =  [sum(y) for y in zip( new_center, [x*(mem[j][i]**m) for x in df[i]] )]
+            denominator += (mem[j][i]**m)
+        new_centroid.append([numerator/denominator for numerator in new_center])
+    return new_centroid
+
+def calcObjFunction(df,mem, centroid, dataSize, k, m):
+    obj_func = 0
+    for i in range(dataSize):
+        for j in range(k):
+            obj_func += (mem[j][i]**m)*(norm_square(df[i], centroid[j]))
+    return obj_func
+
+def calcDistMatrix(dist, c, df):
+    row = 0
+    for centroid in c: #calculating distance matrix
+        colum = 0
+        for point in df:
+            dist[row][colum] = getDistance(centroid, point)
+            colum+=1
+        row+=1
+    return dist
+
+def stoppingCriteria2(center_diff):
+    check = False
+    for x in center_diff:
+        if x == 0:
+            check = True
+    return check
+
+def caclMembershipMatrix(mem, dist,dataSize,k, m):
+    for i in range(k): 
+        for j in range(dataSize):
+            mem[i][j] = memb(dist, j, i, m)
+    return mem
